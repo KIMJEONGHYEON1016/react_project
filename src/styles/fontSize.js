d55bd00d160e436639c280c1a91d0b57499a4b67
KIMJEONGHYEON1016 <dkemfdk4@naver.com>
2024-04-11 16:29:54 +0900

    공통 스타일, 공통 글자 크기,  공통 색상

---
+++ src/styles/fontSize.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const fontSize = {
+  small: '0.75rem',
+  normall: '1rem',
+  medium: '1.25rem',
+  big: '1.5rem',
+  extraBig: '2rem',
+};
+
+export default React.memo(fontSize);
